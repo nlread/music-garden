34f22f572fc3da09f85c2966b8c7f0c9acaa24fc
Anja Beth Swoap <anja.beth@gmail.com>
2017-02-12 16:52:32 -0600

    Adding AB's Paper practice files + file structure

---
+++ learning/paper/anja-beth/js/dragAndDrop.js
@@ -0,0 +1,71 @@
+paper.install(window); //make paper scope global by injecting it into window - from here http://blog.lindsayelia.com/post/128346565323/making-paperjs-work-in-an-external-file
+
+//run after window has loaded
+window.onload = function(){
+    //sanity check
+    console.log("window loaded");
+    
+    //create canvas first using id
+    paper.setup('canvas')
+    
+    view.draw(); //helps speed up drawing
+    
+    var myTool = new Tool();
+    
+    //drag and drop code adapted from here http://stackoverflow.com/questions/16876253/paperjs-drag-and-drop-circle
+    
+    //need to fill this array with things (flowers)
+    var pink = new Raster('pink');
+    var orange = new Raster('orange');
+    var blue = new Raster('blue');
+    var purple = new Raster('purple');
+    
+    var flowers = [pink, orange, blue, purple];
+    
+    //position flowers along the left side of the canvas
+    xPos = 20
+    yPos = 20
+    for(var i = 0; i < flowers.length; i++){
+        flowers[i].position = new Point(xPos, yPos)
+        yPos += 150 //put them in a vertical line
+    }
+
+    // Mouse tool state
+    var draggingIndex = -1;
+    var createdClone = false;
+    var currentFlower;
+
+    myTool.onMouseDrag = function(event) {
+        //hit test to see if we are on top of a menu flower
+        if (flowers.length > 0) {
+            for (var ix = 0; ix < flowers.length; ix++) {
+                //if you've hit a flower, make the dragging index equal to that flower
+                if (flowers[ix].contains(event.point)) {
+                    draggingIndex = ix;
+                    break;
+                }
+            }
+        }
+
+        //clicked on a menu flower
+        if (draggingIndex > -1) {
+            //check if we already cloned it so that we don't get a million of them. if not, create a clone
+            if(!createdClone){
+                currentFlower = flowers[draggingIndex].clone()
+                createdClone = true;
+            }
+            currentFlower.position = event.point; //BTS will need to reset this later to prevent moving the wrong things
+        } else {
+            //need to handle case where we haven't hit flower - do we need to do anything here?
+             
+        }
+    };
+
+    function onMouseUp(event) {
+        //here need to drop flower at event point
+       
+        // Reset the tool state
+        draggingIndex = -1;
+    };
+}
+
