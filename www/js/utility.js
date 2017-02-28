50f8b4a7b56fba503c86cdb0dff9bbbfce12bae2
nlread <nedlyfordread@gmail.com>
2017-02-28 12:25:21 -0600

    Fixed loading all paths

--- www/js/utility.js
+++ www/js/utility.js
@@ -1,12 +1,28 @@
-var utils {
-    importSVG: function(svgID) {
+class Utils {
+    
+    static importSVG(svgID) {
         return project.importSVG(svgID, {'insert': false});
-    }, 
-    loadPathsFromCSV: function(svgID) {
-        let group = this.importSVG(svgID);
+    }
+    
+    static loadPathsFromSVG(svgID) {
+        let importedData = this.importSVG(svgID);
         let paths = [];
-        for(let i=0; i<group.length; i++) {
-            paths.push
+        let groups = [];
+        groups.push(importedData);
+        
+        while(groups.length > 0) {
+            let group = groups.pop();
+            for(let i=0; i<group.children.length; i++) {
+                let obj = group.children[i]; 
+                console.log(typeof(obj));
+                if (obj instanceof Path) {
+                    paths.push(obj);
+                } else if(obj instanceof Group) {
+                    groups.push(obj);
+                }
+            }
         }
-    }, 
-}
+        
+        return paths;
+    }
+}   
