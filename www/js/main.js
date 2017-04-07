9febbcb9b4ec6596783f17a91573e52a8dd472d6
Anja Beth Swoap <anja.beth@gmail.com>
2017-04-07 11:39:16 -0500

    Fixed syntax error, animations are currently on

--- www/js/main.js
+++ www/js/main.js
@@ -371,9 +371,10 @@ dropFlower = function(clickEvent){
         newFlower.music.sound.on('play', function() {
             console.log("played");
             
-//            Animation 1: Gets bigger then smaller, kind of like a pop. Could also reverse it.
+        //Animation 1: Gets bigger then smaller, kind of like a pop. Could also reverse it.
            newFlower.animate(new ScalingAnimation(new Point(1.3,1.3),0.5,0));
-           newFlower.animate(new ScalingAnimation(new Point(1/1.3,1/1.3),1,0));}
+           newFlower.animate(new ScalingAnimation(new Point(1/1.3,1/1.3),1,0));
+        });
         
         canvasFlowers[mouseStates.currentFlower.img.id] = newFlower;
     } 
